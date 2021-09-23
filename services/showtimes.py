74ffd5e25f5e018b2ba846141156c2e20daeb548
wangso <wangso@missouri.edu>
2021-09-23 16:54:39 -0500

    Update showtimes.py

--- services/showtimes.py
+++ services/showtimes.py
@@ -34,4 +34,4 @@ def showtimes_record(date):
     return nice_json(showtimes[date])
 
 if __name__ == "__main__":
-    app.run(port=5002, debug=True)
+    app.run(port=5002, host='0.0.0.0', debug=True)
